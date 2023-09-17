a9a4f7067d8934eb1e26c80657e02552e1727b6b
R-Kershaw <rkershaw90@gmail.com>
2023-09-17 19:47:47 -0400

    Allowed only products of a certain category to be rendered on the page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,22 +16,22 @@ export default function Home() {
 
             const categoryData = await getAllCategories();
             setAllCategories(categoryData);
-            
+
             //   const dataSingleProduct = await getSingleProduct('2');
             //  setSingleProduct(dataSingleProduct);
-            
+
             const productsInCategoryData = await getProductsInCategory('electronics');
             setProductsInCategory(productsInCategoryData);
-            
+
             setLoading(false);
         }
         fetchData();
     }, []);
 
-    //   console.log(allProducts);
+    console.log(allProducts);
     //   console.log(singleProduct);
-    console.log(allCategories);
-    console.log(productsInCategory);
+    //   console.log(allCategories);
+    //   console.log(productsInCategory);
 
     if (loading) {
         return (
@@ -41,6 +41,22 @@ export default function Home() {
         );
     }
 
+    //use with the category navbar
+    async function showProductsInCategory(categoryName) {
+        if (allCategories.includes(categoryName)) {
+            const productsInCategoryData = await getProductsInCategory(categoryName);
+            setAllProducts(productsInCategoryData);
+        }
+        else
+        {
+            //if the category name is invalid or doesn't exist, just show all items
+            const productsInCategoryData = await getAllProducts();
+            setAllProducts(productsInCategoryData);
+            console.log("value does not exists in this object");
+        }
+        console.log(allProducts);
+    }
+
     return (
         <>
             <h1>
@@ -48,6 +64,7 @@ export default function Home() {
             </h1>
             <br></br>
             <div>
+                <button onClick={() => showProductsInCategory('electronics')}>Electronics</button>
                 {allProducts.map(product => (
                     <Product key={product.id} product={product} />
                 ))}
