bc71176992ee46ff2305fedc275830df5a486417
R-Kershaw <rkershaw90@gmail.com>
2023-09-16 20:33:53 -0400

    Added template restful API functions and now able to view all products

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,98 @@
 //export restful functions to be used elsewhere
+//use the fakeStoreAPI for now but replace the code when the custom backend is completed
+
+/*** products ***/
+//GET
+export async function getAllProducts() {
+    try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/*** cart ***/
+//GET
+export async function getCartItems() {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//POST
+export async function addCartItem(item) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//PATCH
+export async function editCartItem(item) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//DELETE
+export async function deleteCartItem(item) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+/*** orders ***/
+//GET
+export async function getAllOrders() {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//GET
+export async function getOrder(order) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+/*** users ***/
+//POST
+export async function registerUser(username, password) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//POST
+export async function signInUser(username, password) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+//GET
+export async function getCurrentUser() {
+    try {
+
+    } catch (error) {
+
+    }
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,40 @@
+import { getAllProducts } from "../api";
+import { useEffect, useState } from "react";
+import Product from "../components/Product";
+
 export default function Home() {
+    const [allProducts, setAllProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getAllProducts();
+            setAllProducts(data);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    console.log(allProducts);
+
+    if (loading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        );
+    }
+
     return (
         <>
             <h1>
                 Home Page
             </h1>
+            <div>
+                {allProducts.map(product => (
+                    <Product key={product.id} product={product} />
+                ))}
+            </div>
         </>
     )
 }
