2c2d1c277213b9d7956f3d904ae046988d91bbf4
R-Kershaw <rkershaw90@gmail.com>
2023-09-19 23:27:36 -0400

    Added sign in functionality

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -117,7 +117,19 @@ export async function registerUser(username, password) {
 //POST
 export async function signInUser(username, password) {
     try {
-
+        const response = await fetch(`${BASE_API_URL}/auth/login`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+        const result = await response.json();
+        console.log(result);
+        return result;
     } catch (error) {
         console.log(error);
     }
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,56 @@
+import { json, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { signInUser } from "../api";
+
 export default function SignIn() {
+    const [username, setUsername] = useState("");
+    const [password, setPasssword] = useState("");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            const userData = await signInUser(username, password);
+            console.log(userData);
+            //set userData in authContext and localStorage
+            if (userData.token) {
+                navigate(`/`);
+            }
+            else {
+                setError(json.error);
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
     return (
         <>
             <h1>
                 Sign In Page
             </h1>
+            <br></br>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Username:{""}
+                        <input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Password:{""}
+                        <input type="password"
+                            value={password}
+                            onChange={(e) => setPasssword(e.target.value)}
+                        />
+                    </label>
+                    <button>Sign In</button>
+                </form>
+            </div>
         </>
     )
 }
