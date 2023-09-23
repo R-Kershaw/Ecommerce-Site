5f1e74dc9a0be3af76c8e39a5f88effbb37a59c1
R-Kershaw <rkershaw90@gmail.com>
2023-09-22 22:59:47 -0400

    Added editing cart functionality

--- frontend/src/contexts/CartContext.jsx
+++ frontend/src/contexts/CartContext.jsx
@@ -15,33 +15,32 @@ export const CART_ACTION = {
             return { ...state } */
 
 export const cartReducer = (state, action) => {
+    //gets a reference to the object to directly modify its properties
+    let foundProduct = state.cart.find((product) => {
+        return product.id === action.payload.id;
+    });
+
     switch (action.type) {
         case CART_ACTION.FETCH_CART:
             break;
-        case CART_ACTION.ADD_PRODUCT:
-            console.log(action);
-            
-            //gets a reference to the object to directly modify the value
-            let newProduct = state.cart.find((product) => {
-                return product.id === action.payload.id;
-            });
-            if (newProduct) {
-                console.log("Product already exists");
-                //   console.log(newProduct);
-                newProduct.quantity += action.payload.quantity;
-                // console.log(newProduct);
-                return { ...state, cart: [...state.cart]  } //update the newly modified product that was found
+        case CART_ACTION.ADD_PRODUCT: //if a product already exists in the cart then update its quantity property, otherwise add the new product to the end of the cart
+            //update the newly modified product that was found
+            if (foundProduct) {
+                foundProduct.quantity += action.payload.quantity;
+                return { ...state, cart: [...state.cart] }
+            }
+            //add the new product to the end of the cart since it doesn't exist yet
+            else {
+                return { ...state, cart: [...state.cart, { id: action.payload.id, quantity: action.payload.quantity }] }
+            }
+        case CART_ACTION.EDIT_PRODUCT: //you can only edit products that exist in the cart
+            if (foundProduct) {
+                foundProduct.quantity = action.payload.quantity;
+                return { ...state, cart: [...state.cart] }
             }
             else {
-                console.log("Product does not exist yet");
-                return { ...state, cart: [...state.cart, { id: action.payload.id, quantity: action.payload.quantity }] } //add the latest item to the end of the cart
+                return { ...state }
             }
-            console.log(newProduct);
-
-        case CART_ACTION.EDIT_PRODUCT:
-            //    console.log(state.cart.)
-            return { ...state, cart: [...state.cart, { id: action.payload.id, quantity: action.payload.quantity }] }
-            break;
         case CART_ACTION.DELETE_PRODUCT:
             break;
         default:
