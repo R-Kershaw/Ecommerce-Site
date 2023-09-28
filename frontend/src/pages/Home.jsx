0c223dfe3b190a5a2f36efa10de569c643c2040b
R-Kershaw <rkershaw90@gmail.com>
2023-09-28 01:25:06 -0400

    Added fully functioning sorting and filtering by category and price. Got rid of API category code because it was significantly slower and inefficient

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,12 @@ export default function Home() {
     const [allCategories, setAllCategories] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const [selectedCategory, setSelectedCategory] = useState("default");
+    const [selectedSort, setSelectedSort] = useState("default");
+    const [selectedFilter, setSelectedFilter] = useState("default");
+    const [filterMin, setFilterMin] = useState(0);
+    const [filterMax, setFilterMax] = useState(0);
+
     //helper object to prevent spelling errors
     const SORT_TYPE = {
         PRICE_ASCENDING: 'priceAscending',
@@ -18,7 +24,11 @@ export default function Home() {
     }
 
     const FILTER_TYPE = {
-        PRICE: 'price',
+        PRICE_0_25: 'price_0_25',
+        PRICE_25_50: 'price_25_50',
+        PRICE_50_100: 'price_50_100',
+        PRICE_100_200: 'price_100_200',
+        PRICE_200_PLUS: 'price_200_PLUS',
         RATING: 'rating',
         REVIEWS: 'reviews'
     }
@@ -36,27 +46,22 @@ export default function Home() {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        sortProductsByType(selectedSort);
+    }, [selectedSort]);
+
+    useEffect(() => {
+        console.log(filterMin, filterMax);
+        console.log(allProducts.filter(product => product.category === selectedCategory && product.price >= filterMin && product.price <= filterMax));
+        filterProductsByType(selectedFilter);
+    }, [selectedFilter]);
+
     if (loading) {
         return (
             <div>
                 <h1>Loading...</h1>
             </div>
         );
-    }
-
-    //use with the category navbar
-    async function showProductsInCategory(categoryName) {
-        if (allCategories.includes(categoryName)) {
-            const productsInCategoryData = await getProductsInCategory(categoryName);
-            setAllProducts(productsInCategoryData);
-        }
-        else {
-            //if the category name is invalid or doesn't exist, just show all items
-            const productsInCategoryData = await getAllProducts();
-            setAllProducts(productsInCategoryData);
-            console.log("value does not exists in this object");
-        }
-        console.log(allProducts);
     }
 
     //sort by price, rating, and review count
@@ -83,51 +88,87 @@ export default function Home() {
                 sortedProducts = [...allProducts].sort((a, b) => a.rating.count < b.rating.count ? 1 : -1);
                 break;
             default:
-                throw new Error();
+                break;
         }
         setAllProducts(sortedProducts);
     }
 
     //filter by price, rating, and review count based on a min and max value
-    function filterProductsByType(type, min, max) {
-        let filteredProducts = [...allProducts];
+    /*    function filterProductsByType(type, min, max) {
+            let filteredProducts = [...allProducts];
+            switch (type) {
+                case FILTER_TYPE.PRICE:
+                    filteredProducts = [...allProducts].filter((product) => product.price >= min && product.price <= max);
+                    break;
+                case FILTER_TYPE.RATING:
+                    filteredProducts = [...allProducts].filter((product) => product.rating.rate >= min && product.rating.rate <= max);
+                    break;
+                case FILTER_TYPE.REVIEWS:
+                    filteredProducts = [...allProducts].filter((product) => product.rating.count >= min && product.rating.count <= max);
+                    break;
+                default:
+                    break;
+            }
+            setAllProducts(filteredProducts);
+            console.log(filteredProducts);
+        }
+        function filterProductsByType(type) {
+            let filteredProducts = [...allProducts];
+            switch (type) {
+                case FILTER_TYPE.PRICE:
+                    filteredProducts = [...allProducts].filter((product) => product.price >= min && product.price <= max);
+                    break;
+                case FILTER_TYPE.RATING:
+                    filteredProducts = [...allProducts].filter((product) => product.rating.rate >= min && product.rating.rate <= max);
+                    break;
+                case FILTER_TYPE.REVIEWS:
+                    filteredProducts = [...allProducts].filter((product) => product.rating.count >= min && product.rating.count <= max);
+                    break;
+                default:
+                    break;
+            }
+            setAllProducts(filteredProducts);
+            console.log(filteredProducts);
+        }*/
+
+    function filterProductsByType(type) {
+        console.log('entered');
         switch (type) {
-            case FILTER_TYPE.PRICE:
-                filteredProducts = [...allProducts].filter((product) => product.price >= min && product.price <= max);
+            case FILTER_TYPE.PRICE_0_25:
+                setFilterMin(0);
+                setFilterMax(25);
                 break;
-            case FILTER_TYPE.RATING:
-                filteredProducts = [...allProducts].filter((product) => product.rating.rate >= min && product.rating.rate <= max);
+            case FILTER_TYPE.PRICE_25_50:
+                setFilterMin(25);
+                setFilterMax(50);
                 break;
-            case FILTER_TYPE.REVIEWS:
-                filteredProducts = [...allProducts].filter((product) => product.rating.count >= min && product.rating.count <= max);
+            case FILTER_TYPE.PRICE_50_100:
+                setFilterMin(50);
+                setFilterMax(100);
+                break;
+            case FILTER_TYPE.PRICE_100_200:
+                setFilterMin(100);
+                setFilterMax(200);
+                break;
+            case FILTER_TYPE.PRICE_200_PLUS:
+                setFilterMin(200);
+                setFilterMax(Number.MAX_SAFE_INTEGER);
                 break;
             default:
-                throw new Error();
+                setFilterMin(0);
+                setFilterMax(Number.MAX_SAFE_INTEGER);
+                break;
         }
-        setAllProducts(filteredProducts);
-        console.log(filteredProducts);
     }
 
     return (
         <>
             <div className="m-2">
-                <button onClick={() => showProductsInCategory('electronics')}>Electronics</button>
-                {/*   
-                <button onClick={() => sortProductsByType(SORT_TYPE.PRICE_ASCENDING)}>Price Ascending</button>
-                <button onClick={() => sortProductsByType(SORT_TYPE.PRICE_DESCENDING)}>Price Descending</button>
-                <button onClick={() => sortProductsByType(SORT_TYPE.RATING_ASCENDING)}>Rating Ascending</button>
-                <button onClick={() => sortProductsByType(SORT_TYPE.RATING_DESCENDING)}>Rating Descending</button>
-                <button onClick={() => sortProductsByType(SORT_TYPE.REVIEWS_ASCENDING)}>Reviews Ascending</button>
-                <button onClick={() => sortProductsByType(SORT_TYPE.REVIEWS_DESCENDING)}>Reviews Descending</button>
-                */}
-                <button onClick={() => filterProductsByType(FILTER_TYPE.PRICE, 0, 25)}>Filter By Price</button>
-                <button onClick={() => filterProductsByType(FILTER_TYPE.RATING, 4, 5)}>Filter By Rating</button>
-                <button onClick={() => filterProductsByType(FILTER_TYPE.REVIEWS, 100, 300)}>Filter By Reviews</button>
                 <div className="m-1 grid grid-cols-8 gap-x-2 gap-y-4">
-
                     <select className="col-span-3 capitalize border bg-trf-50 font-bold py-2 px-2 rounded shadow-md"
-                        onChange={(e) => {showProductsInCategory(e.target.value)}}>
-                        <option value="all"> category: all</option>
+                        value={selectedCategory}
+                        onChange={(e) => { setSelectedCategory(e.target.value) }}>
+                        <option value="default"> category: all</option>
                         {allCategories.map((category) =>
                             <option value={category}>
                                 {category}
@@ -136,12 +177,10 @@ export default function Home() {
                 </div>
 
                 <div className="m-1 grid grid-cols-8 gap-x-2 gap-y-4">
-
                     <select className="col-span-3 capitalize border bg-trf-50 font-bold py-2 px-2 rounded shadow-md"
-                        onChange={(e) => {
-                            sortProductsByType(e.target.value)
-                        }}>
-                        <option value="all"> sort by: all</option>
+                        value={selectedSort}
+                        onChange={(e) => { setSelectedSort(e.target.value) }}>
+                        <option value="default"> sort by: all</option>
                         <option value={SORT_TYPE.PRICE_ASCENDING}> Price Ascending</option>
                         <option value={SORT_TYPE.PRICE_DESCENDING}> Price Descending</option>
                         <option value={SORT_TYPE.RATING_ASCENDING}> Rating Ascending</option>
@@ -151,13 +190,53 @@ export default function Home() {
                     </select>
                 </div>
 
+                <div className="m-1 grid grid-cols-8 gap-x-2 gap-y-4">
+                    <select className="col-span-3 capitalize border bg-trf-50 font-bold py-2 px-2 rounded shadow-md"
+                        value={selectedFilter}
+                        onChange={(e) => { setSelectedFilter(e.target.value) }}>
+                        <option value="default"> filter by: none</option>
+                        <option value={FILTER_TYPE.PRICE_0_25}> Under $25</option>
+                        <option value={FILTER_TYPE.PRICE_25_50}> $25 to $50</option>
+                        <option value={FILTER_TYPE.PRICE_50_100}> $50 to $100</option>
+                        <option value={FILTER_TYPE.PRICE_100_200}> $100 to $200</option>
+                        <option value={FILTER_TYPE.PRICE_200_PLUS}> $200 & Above</option>
+                    </select>
+                </div>
+
                 <div className="my-2 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4">
-                    {allProducts.map(product => (
+                    {/*allProducts.map(product => (
                         <ProductCard
                             key={product.id}
                             product={product}
                         />
-                    ))}
+                    ))*/}
+                    {selectedCategory !== 'default' && selectedFilter !== 'default' //if the category and filter are both selected
+                        ? allProducts.filter(product => product.category === selectedCategory && product.price >= filterMin && product.price <= filterMax).map(product => (
+                            <ProductCard
+                                key={product.id}
+                                product={product}
+                            />
+                        ))
+                        : selectedCategory !== 'default' //if the category is selected only
+                            ? allProducts.filter(product => product.category === selectedCategory).map(product => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))
+                            : selectedFilter !== 'default' //if the filter is set only
+                                ? allProducts.filter(product => product.price >= filterMin && product.price <= filterMax).map(product => (
+                                    <ProductCard
+                                        key={product.id}
+                                        product={product}
+                                    />))
+                                : //default condition when no category or filter is selected
+                                allProducts.map(product => (
+                                    <ProductCard
+                                        key={product.id}
+                                        product={product}
+                                    />))
+                    }
                 </div>
             </div>
         </>
